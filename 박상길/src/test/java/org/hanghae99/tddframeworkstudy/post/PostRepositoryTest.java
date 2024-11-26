23fd7f5d6aa8fd9c4448d619811e605e29c38121
gil <fkdl3919@gmail.com>
2024-11-27 00:53:41 +0900

    test: 게시글 작성 API 테스트 작성 data

--- 박상길/src/test/java/org/hanghae99/tddframeworkstudy/post/PostRepositoryTest.java
+++ 박상길/src/test/java/org/hanghae99/tddframeworkstudy/post/PostRepositoryTest.java
@@ -1,11 +1,6 @@
 package org.hanghae99.tddframeworkstudy.post;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
@@ -13,10 +8,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
 @DataJpaTest
 public class PostRepositoryTest {
@@ -24,21 +15,32 @@ public class PostRepositoryTest {
     @Autowired
     private PostRepository postRepository;
 
+    private final String TITLE1 = "test1";
+    private final String AUTHOR1 = "gil1";
+    private final String CONTENTS1 = "작성 내용";
+    private final LocalDateTime LOCAL_DATE_TIME1 = LocalDateTime.of(2024, 11, 25, 0, 0);
+
+    private final String TITLE2 = "test2";
+    private final String AUTHOR2 = "gil2";
+    private final String CONTENTS2 = "작성 내용2";
+    private final LocalDateTime LOCAL_DATE_TIME2 = LocalDateTime.of(2024, 11, 24, 0, 0);
+
+
     @Test
     @DisplayName("전체 게시글 목록 조회 API")
     public void selectAllPost() throws Exception {
         // given
         Post post1 = new Post();
-        post1.setTitle("test1");
-        post1.setAuthor("gil1");
-        post1.setContents("작성 내용");
-        post1.setCreatedAt(LocalDateTime.now());
+        post1.setTitle(TITLE1);
+        post1.setAuthor(AUTHOR1);
+        post1.setContents(CONTENTS1);
+        post1.setCreatedAt(LOCAL_DATE_TIME1);
 
         Post post2 = new Post();
-        post2.setTitle("test2");
-        post2.setAuthor("gil2");
-        post2.setContents("작성 내용");
-        post2.setCreatedAt(LocalDateTime.now().minusDays(1));
+        post2.setTitle(TITLE2);
+        post2.setAuthor(AUTHOR2);
+        post2.setContents(CONTENTS2);
+        post2.setCreatedAt(LOCAL_DATE_TIME2);
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
@@ -47,9 +49,34 @@ public class PostRepositoryTest {
 
         // then
         assertThat(posts.size()).isEqualTo(2);
-        assertThat(posts.get(0).getTitle()).isEqualTo("test1");
-        assertThat(posts.get(1).getTitle()).isEqualTo("test2");
+        assertThat(posts.get(0).getTitle()).isEqualTo(TITLE1);
+        assertThat(posts.get(1).getTitle()).isEqualTo(TITLE2);
 
+    }
+
+    @Test
+    @DisplayName("게시글 작성 data")
+    public void writePost() throws Exception {
+        // given
+        Post post1 = new Post();
+        post1.setTitle(TITLE1);
+        post1.setAuthor(AUTHOR1);
+        post1.setContents(CONTENTS1);
+        post1.setCreatedAt(LOCAL_DATE_TIME1);
+
+        Post post2 = new Post();
+        post2.setTitle(TITLE2);
+        post2.setAuthor(AUTHOR2);
+        post2.setContents(CONTENTS2);
+        post2.setCreatedAt(LOCAL_DATE_TIME2);
+
+        // when
+        Post save = postRepository.save(post1);
+
+        // then
+        // 동치성 검증
+        assertThat(save).isEqualTo(post1);
+        assertThat(save).isNotEqualTo(post2);
 
     }
 
