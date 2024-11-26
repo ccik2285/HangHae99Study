f53094aaa9cad94450f4d16a485a14edb9ce2f49
gil <fkdl3919@gmail.com>
2024-11-26 22:39:47 +0900

    feat: base 및 auditing 기능 추가

---
+++ 박상길/src/main/java/org/hanghae99/tddframeworkstudy/common/entity/Base.java
@@ -0,0 +1,24 @@
+package org.hanghae99.tddframeworkstudy.common.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Base {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    protected LocalDateTime updatedAt;
+
+}
