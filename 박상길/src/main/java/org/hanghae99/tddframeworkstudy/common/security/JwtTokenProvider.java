2112d065e789ec41e1b0de673d1e30ec2513930d
gil <fkdl3919@gmail.com>
2024-12-03 18:14:28 +0900

    feat: 토큰생성 succ test 작성

--- 박상길/src/main/java/org/hanghae99/tddframeworkstudy/common/security/JwtTokenProvider.java
+++ 박상길/src/main/java/org/hanghae99/tddframeworkstudy/common/security/JwtTokenProvider.java
@@ -1,2 +1,27 @@
-package org.hanghae99.tddframeworkstudy.common.security;public class JwtTokenProvider {
+package org.hanghae99.tddframeworkstudy.common.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class JwtTokenProvider {
+
+    private final SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256); // 비밀키
+    private final long EXPIRATION_TIME = 3600000; // 1시간
+
+    // JWT 토큰 생성
+    public String generateToken(String username) {
+        return Jwts.builder()
+            .setSubject(username)
+            .setIssuedAt(new Date()) // 토큰 발행 시간
+            .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME)) // 토큰 만료 시간
+            .signWith(SECRET_KEY) // 비밀키로 서명
+            .compact();
+    }
 }
