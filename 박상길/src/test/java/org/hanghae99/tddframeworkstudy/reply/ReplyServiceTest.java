497b607a777cbc40598a51dfc366b654fc062771
gil <fkdl3919@gmail.com>
2024-12-10 01:58:46 +0900

    test: 댓글수정 api fail 테스트 작성 service

--- 박상길/src/test/java/org/hanghae99/tddframeworkstudy/reply/ReplyServiceTest.java
+++ 박상길/src/test/java/org/hanghae99/tddframeworkstudy/reply/ReplyServiceTest.java
@@ -14,6 +14,7 @@ import org.hanghae99.tddframeworkstudy.post.Post;
 import org.hanghae99.tddframeworkstudy.post.PostDto;
 import org.hanghae99.tddframeworkstudy.post.PostRepository;
 import org.hanghae99.tddframeworkstudy.user.User;
+import org.hanghae99.tddframeworkstudy.user.UserDto;
 import org.hanghae99.tddframeworkstudy.user.UserRole;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -41,6 +42,7 @@ public class ReplyServiceTest {
     // reply
     private final Long REPLY_ID = 1L;
     private final Long REPLY_ID2 = 2L;
+    private final Long REPLY_ID3 = 3L;
     private final String REPLY_CONTENTS = "test";
     private final String TEST_USER_NAME = "test";
     private final Long TEST_USER_ID = 1L;
@@ -122,6 +124,74 @@ public class ReplyServiceTest {
 
         verify(replyRepository, times(1)).save(any());
 
+    }
+
+    /**
+     * 댓글 수정 API 구현 요구사항
+     *  토큰을 검사한 후, 유효한 토큰이면서 해당 사용자가 작성한 댓글만 수정 가능
+     *  선택한 댓글의 DB 저장 유무를 확인
+     *  선택한 댓글이 있다면 댓글 수정하고 수정된 댓글 반환
+     */
+    @Test
+    @DisplayName("댓글수정 api controller")
+    public void updateReply() throws Exception {
+        // given
+        UserDto userDto = new UserDto();
+        userDto.setId(TEST_USER_ID);
+        userDto.setName(TEST_USER_NAME);
+        userDto.setRole(UserRole.ADMIN);
+
+        String validToken = jwtTokenProvider.generateToken(userDto.getName(), userDto.getId());
+        String invalidToken = jwtTokenProvider.generateToken(userDto.getName(), 2L);
+
+        // reply 1 - for succ
+        ReplyDto replyDto = new ReplyDto();
+        replyDto.setId(REPLY_ID);
+        replyDto.setContents(REPLY_CONTENTS);
+        replyDto.setUserDto(userDto);
+        Reply reply = new Reply(replyDto);
+
+        // reply 2 - entity not found
+        ReplyDto replyDto2 = new ReplyDto();
+        replyDto2.setId(REPLY_ID2);
+        replyDto2.setContents(REPLY_CONTENTS);
+        Reply reply2 = new Reply(replyDto2);
+
+        // reply 3 - invalid token
+        ReplyDto replyDto3 = new ReplyDto();
+        replyDto3.setId(REPLY_ID3);
+        replyDto3.setContents(REPLY_CONTENTS);
+        replyDto3.setUserDto(userDto);
+        Reply reply3 = new Reply(replyDto3);
+
+
+        // stub
+        when(replyRepository.findById(REPLY_ID)).thenReturn(Optional.of(reply));
+        when(replyRepository.findById(REPLY_ID2)).thenReturn(Optional.empty());
+        when(replyRepository.findById(REPLY_ID3)).thenReturn(Optional.of(reply3));
+
+        when(replyRepository.save(any())).thenReturn(reply);
+
+        // when then
+        /**
+         * 토큰을 검사한 후, 유효한 토큰이면서 해당 사용자가 작성한 댓글만 수정 가능
+         *
+         * 유저와 토큰 불일치
+         */
+        assertThatThrownBy(() -> replyService.update(replyDto3, invalidToken)).isInstanceOf(JwtException.class);
+
+        /**
+         * 선택한 댓글의 DB 저장 유무를 확인
+         */
+        assertThatThrownBy(() -> replyService.update(replyDto2, validToken)).isInstanceOf(EntityNotFoundException.class);
+
+        /**
+         * 선택한 댓글이 있다면 댓글 수정하고 수정된 댓글 반환
+         */
+        ReplyDto saveReply = replyService.update(replyDto, validToken);
+
+        assertThat(saveReply.getId()).isEqualTo(REPLY_ID);
+        verify(replyRepository, times(1)).save(any());
     }
 
 }
