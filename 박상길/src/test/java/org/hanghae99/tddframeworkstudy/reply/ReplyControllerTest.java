4440263dd7c642c2f69cca556c1f3bf72db0f30c
gil <fkdl3919@gmail.com>
2024-12-10 01:33:31 +0900

    test: 댓글수정 api fail 테스트 작성 controller

--- 박상길/src/test/java/org/hanghae99/tddframeworkstudy/reply/ReplyControllerTest.java
+++ 박상길/src/test/java/org/hanghae99/tddframeworkstudy/reply/ReplyControllerTest.java
@@ -4,11 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import org.hanghae99.tddframeworkstudy.common.security.JwtTokenProvider;
+import org.hanghae99.tddframeworkstudy.user.UserDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +32,9 @@ public class ReplyControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    private final Long USER_ID = 1L;
+    private final String USER_NAME = "gil";
 
     private final String REPLY_CONTENTS = "test";
 
@@ -64,6 +70,44 @@ public class ReplyControllerTest {
         ReplyDto returnVal = objectMapper.readValue(objectMapper.writeValueAsString(readValue.get("data")), ReplyDto.class);
 
         assertThat(returnVal.getContents()).isEqualTo(REPLY_CONTENTS);
+    }
+
+    /**
+     * 댓글 수정 API 구현 요구사항
+     *  토큰을 검사한 후, 유효한 토큰이면서 해당 사용자가 작성한 댓글만 수정 가능
+     *  선택한 댓글의 DB 저장 유무를 확인
+     *  선택한 댓글이 있다면 댓글 수정하고 수정된 댓글 반환
+     */
+    @Test
+    @DisplayName("댓글수정 api controller")
+    public void updateReply() throws Exception {
+        String url = "/1";
+
+        // given
+        UserDto userDto = new UserDto();
+        userDto.setId(USER_ID);
+        userDto.setName(USER_NAME);
+
+        ReplyDto replyDto = new ReplyDto();
+        replyDto.setContents(REPLY_CONTENTS);
+        Reply reply = new Reply(replyDto);
+
+        JwtTokenProvider jwtTokenProvider = new JwtTokenProvider();
+        String token = jwtTokenProvider.generateToken(userDto.getName(), userDto.getId());
+
+        when(replyService.update(replyDto)).thenReturn(reply);
+
+        // when then
+        mockMvc.perform(put(url)
+                .header("Authorization", "Bearer " + token)
+            .content(objectMapper.writeValueAsString(replyDto)).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+
+        // then
+        mockMvc.perform(put(url))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.status").value("error"));
+
     }
 
 }
